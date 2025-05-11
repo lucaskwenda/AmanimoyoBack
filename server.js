8c2b7c65b7e66c9ac10659e33ca42239f7da77c6
Lucas Kwenda <133110595+lucaskwenda@users.noreply.github.com>
2025-05-11 01:25:02 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ import { fileTypeFromBuffer } from 'file-type';
 
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: 'https://lucaskwenda.github.io'
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: 'https://lucaskwenda.github.io'
+// }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
